72bd86cf8b8e9cc469365f54e8886227a02d26a0
amirmohammadgholampour <amirmohammadghlmpr@gmail.com>
2026-02-19 13:17:09 +0330

    add python file

---
+++ LeNet-5.py
@@ -0,0 +1,67 @@
+import torch.nn as nn
+
+class LeNet5(nn.Module): 
+    def __init__(self) -> None:
+        super().__init__()
+
+        # =================
+        # Convolution Layer
+        # =================
+
+        self.feature_extractor = nn.Sequential(
+            # ===========
+            # First Layer 
+            # ===========
+
+            nn.Conv2d(
+                in_channels=1, 
+                out_channels=6, 
+                kernel_size=5, 
+                stride=1, 
+                padding=0
+            ), 
+            nn.AvgPool2d(kernel_size=2, stride=2), 
+            nn.ReLU(), 
+
+            # ============
+            # Second Layer 
+            # ============
+
+            nn.Conv2d(
+                in_channels=6, 
+                out_channels=16, 
+                kernel_size=5, 
+                stride=1, 
+                padding=0
+            ), 
+            nn.AvgPool2d(kernel_size=2, stride=2), 
+            nn.ReLU() 
+        )
+
+        # =====================
+        # Fully Connected Layer
+        # =====================
+
+        self.fully_connected = nn.Sequential(
+            nn.Flatten(), 
+
+            nn.Linear(
+                in_features=16 * 5 * 5, 
+                out_features=120
+            ), 
+
+            nn.Linear(
+                in_features=120, 
+                out_features=84
+            ), 
+
+            nn.Linear(
+                in_features=84, 
+                out_features=10 
+            )
+        )
+
+    def forward(self, x): 
+        x = self.feature_extractor(x)
+        logits = self.fully_connected(x) 
+        return logits 
